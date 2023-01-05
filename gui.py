7de8f2efbb148f120019c255fc716be50b50a6dc
jqwez <jqwez@icloud.com>
2023-01-05 18:16:50 +0000

    Basic GUI

---
+++ gui.py
@@ -0,0 +1,64 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "CodingJQ Youtube Downloader"
+    page.vertical_alignment = "center"
+    page.window_height = 800
+    page.window_width = 600
+
+
+
+    def pick_file_result(e: ft.FilePickerResultEvent):
+        selected_path.value = pick_file_dialog.result.path + ".mp4"
+        
+        page.update()
+
+
+    def handle_submit(e):
+        if not url_input.value:
+            url_input.error_text = "Please enter URL"
+            page.update()
+        else:
+            url_input.error_text = None
+            page.update()
+            pass
+    
+
+    url_input = ft.TextField(label="Video URL")
+    pick_file_dialog = ft.FilePicker(on_result=pick_file_result)
+    pick_file_button = ft.ElevatedButton(
+        "Pick File Name",
+        icon=ft.icons.UPLOAD_FILE,
+        on_click=lambda _: pick_file_dialog.save_file(
+            file_type="video", 
+            allowed_extensions=["mp4"])
+        )
+    page.overlay.append(pick_file_dialog)
+    selected_path = ft.Text()
+    selected_path.value = "File Save Location"
+
+    download_button = ft.TextButton("Download", on_click=handle_submit)
+
+    page.add(
+        ft.Column(
+            [   
+                ft.Row([
+                    url_input
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+                ),
+                ft.Row([
+                    pick_file_button,
+                    selected_path
+                    ]),
+                ft.Row([
+                    download_button,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+                    )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+ft.app(target=main)
